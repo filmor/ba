b5a51755075bcc26d091dda8ff8e04e24f5837ad
Benedikt Sauer <filmor@gmail.com>
2009-06-30 21:17:36 +0200

    test.hpp funktioniert und main.cpp leicht angepasst.

--- prog/main.cpp
+++ prog/main.cpp
@@ -1,15 +1,15 @@
 #include "Analyse.hpp"
 
+#include <stdexcept>
+
 int main (int argc, char** argv)
 {
-    TFile* f = 0;
-    TTree* t = 0;
-    if (argc == 2)
-    {
-        f = new TFile(argv[1]);
-        t = (TTree*)gDirectory->Get("ControlSample0");
-    }
-        
+    if (argc != 2)
+        throw std::runtime_error ("Aye");
+
+    TFile f = new TFile(argv[1]);
+    t = reinterpret_cast<TTree*> (gDirectory->Get ("ControlSample0"));
+
     ba::Analyse a (t);
-    a.Loop();
+    a.Loop ();
 }
--- prog/test.hpp
+++ prog/test.hpp
@@ -4,13 +4,15 @@
 #include <TTree.h>
 
 #include <boost/preprocessor.hpp>
-#include <boost/fusion/sequence.hpp>
+#include <boost/fusion/container/generation/vector_tie.hpp>
 #include <boost/fusion/algorithm.hpp>
 #include <boost/array.hpp>
 #include <boost/fusion/adapted/array.hpp>
+#include <boost/fusion/view/zip_view.hpp>
 
 #include <utility>
 #include <string>
+#include <iostream>
 
 namespace ba
 {
@@ -27,34 +29,34 @@ namespace ba
 #define GET_NAME_STRING(s, data, elem)                              \
     BOOST_PP_STRINGIZE(GET_NAME(s, data, elem))
 
-// Usage: TEST_PARAMETERS((type, name) (type, name) (type, name) ...)
-#define PARAM(type, name)                                           \
-    ((type, name))
-
 #define TEST_PARAMETERS(seq)                                        \
     BOOST_PP_SEQ_FOR_EACH(DO_MEMBER, _, seq)                        \
                                                                     \
-    typedef BOOST_PP_CAT(boost::fusion::vector,                     \
-                         BOOST_PP_SEQ_SIZE(seq))<                   \
-        BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_TRANSFORM(GET_TYPE, 0, seq)) \
-    > parameter_types;                                              \
-    parameter_types get_parameter_types ()                          \
+public:                                                             \
+    void initialize(TTree* tree)                                    \
     {                                                               \
-        return parameter_types (                                    \
-        BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_TRANSFORM(GET_NAME, 1, seq)) \
-                );                                                  \
-    }                                                               \
-    static const boost::array<std::string, BOOST_PP_SEQ_SIZE(seq)>  \
-        get_parameter_names()                                       \
-    {                                                               \
-        static const boost::array<std::string,                      \
-                                  BOOST_PP_SEQ_SIZE(seq)>           \
+        static const                                                \
+            boost::array<const char*, BOOST_PP_SEQ_SIZE(seq)>       \
             names = {{                                              \
             BOOST_PP_SEQ_ENUM(                                      \
                 BOOST_PP_SEQ_TRANSFORM(GET_NAME_STRING, 1, seq)     \
                 )}};                                                \
-        return names;                                               \
-    }
+                                                                    \
+        BOOST_PP_CAT(boost::fusion::vector, BOOST_PP_SEQ_SIZE(seq)) \
+            <                                                       \
+                BOOST_PP_SEQ_ENUM(                                  \
+                    BOOST_PP_SEQ_TRANSFORM(GET_TYPE, 0, seq)        \
+                )                                                   \
+            >                                                       \
+            types (                                                 \
+         BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_TRANSFORM(GET_NAME,1, seq)) \
+               );                                                   \
+        do_initialize(                                              \
+            boost::fusion::vector_tie(names, types),                \
+          tree                                                      \
+        );                                                          \
+    }                                                               \
+private:
 
     namespace
     {
@@ -68,7 +70,10 @@ namespace ba
             template <typename T>
             void operator() (T const& x) const
             {
-                tree_->SetBranchAddress(at_c<0>(x), at_c<1>(x));
+                tree_->SetBranchAddress(
+                            at_c<0>(x),
+                            reinterpret_cast<void*>(&(at_c<1>(x)))
+                        );
             }
         };
     }
@@ -76,14 +81,16 @@ namespace ba
     template <typename DerivedT>
     class test
     {
-    public:
-        void initialize (TTree* tree)
+    protected:
+        template <typename ParametersT>
+        void do_initialize(ParametersT const& parameters, TTree* tree)
         {
             using namespace boost::fusion;
 
-         /*   for_each(zip_view(DerivedT::get_parameter_names(),
-                              DerivedT::get_parameter_types()),
-                     initialize_parameter(tree)); */
+            for_each(
+                zip_view<ParametersT>(parameters),
+                initialize_parameter(tree)
+                );
         }
     };
 
