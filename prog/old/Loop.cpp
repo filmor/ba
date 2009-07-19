0cd68e147b6ff3349facf3b3325fe8a7617f1329
Benedikt Sauer <filmor@gmail.com>
2009-07-19 16:38:52 +0200

    Hauptschleife.

--- prog/old/Loop.cpp
+++ prog/old/Loop.cpp
@@ -9,35 +9,74 @@ namespace ba
     {
         if (fChain == 0) return;
 
-        Long64_t nentries = fChain->GetEntriesFast();
-        TH1D* z_mass = new TH1D("bla", "blubb", 100, 50, 150);
+        Long64_t nentries = 100; // fChain->GetEntriesFast();
+        TH1D* z_mass = new TH1D("z_mass", "Z Masse", 100, 50, 150);
 
         Long64_t nbytes = 0, nb = 0;
-        for (Long64_t jentry=0; jentry<nentries;jentry++) {
+        for (Long64_t jentry=0; jentry<nentries;jentry++)
+        {
             Long64_t ientry = LoadTree(jentry);
             if (ientry < 0) break;
             nb = fChain->GetEntry(jentry);
             nbytes += nb;
 
-            if (El_N == 2 && Mu_N == 0)
+            // Filter Electrons
+            // Filter Muons
+            
+            // Case: e⁺e⁻l
+
+            TLorentzVector z;
+
+            // We need at least 3 leptons
+            if (Mu_N + El_N < 3) continue;
+
+            if (El_N >= 2)
             {
-                TLorentzVector v = get_electron_vector(0) + get_electron_vector(1);
-                z_mass->Fill(v.M() / 1000);
+                // Findet ein e⁺e⁻-Paar mit e⁺ + e⁻ <- Z
+                int i, j;
+                for (i = 0; i < El_N; ++i)
+                {
+                    for (j = i + 1; j < El_N; ++j)
+                    {
+                        if ((*El_charge)[j] + (*El_charge)[i] < 0.2)
+                        {
+                            z = get_electron_vector(i)
+                              + get_electron_vector(j);
+
+                            // Kandidaten speichern
+                            // Zuerst nehmen wir einfach den ersten
+                            // gefundenen it z.M() irgendwo in der Nähe
+                            // von z
+                            z_mass->Fill(z.M() / 1000);
+                        }
+                    }
+                }
+            }
+            else if (Mu_N >= 2)
+            {
+                // Findet ein µ⁺µ⁻-Paar mit µ⁺ + µ⁻ <- Z
+                int i, j;
+                for (i = 0; i < Mu_N; ++i)
+                {
+                    for (j = i + 1; j < Mu_N; ++j)
+                    {
+                        if ((*Mu_charge)[j] + (*Mu_charge)[i] < 0.2)
+                        {
+                            z = get_muon_vector(i)
+                              + get_muon_vector(j);
+
+                            // Kandidaten speichern
+                            // Zuerst nehmen wir einfach den ersten
+                            // gefundenen it z.M() irgendwo in der Nähe
+                            // von z
+                            z_mass->Fill(z.M() / 1000);
+                        }
+                    }
+                }
             }
 
-            if (Mu_N == 2 && El_N == 0)
-            {
-                TLorentzVector v = get_muon_vector(0) + get_muon_vector(1);
-                z_mass->Fill(v.M() / 1000);
-            }
-
-            if (Mu_N == 2 && El_N == 1)
-            {
-            }
-
-            if (El_N == 2 && Mu_N == 1)
-            {
-            }
+            // Drittes Lepton wählen
+            // MET dazunehmen (transversale Masse)
 
         }
 
