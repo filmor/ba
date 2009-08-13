d3488fcb76f898a9bcc9eb13dacd37536d5e76e0
Benedikt Sauer <filmor@gmail.com>
2009-08-13 16:35:11 +0200

    Der Filter war inkorrekt.
    => Müssen nochmal über den gesamten Hintergrund
       laufen. Die Änderungen sind bereits drin,
       aber es fehlen noch die geforderten
       Beweise!

--- analysis/join_background.py
+++ analysis/join_background.py
@@ -10,12 +10,12 @@ signal = [
 target_luminosity = 20000.0 / signal[0][1]
 
 background = [
-        ("output/Np0.root", 900.2),
-        ("output/Np1.root", 205.2),
-        ("output/Np2.root", 69.4),
-        ("output/Np3.root", 21.6),
-        ("output/Np4.root", 6.1),
-        ("output/Np5.root", 1.7)
+        ("output/zmumu_Np0.root", 900.2),
+        ("output/zmumu_Np1.root", 205.2),
+        ("output/zmumu_Np2.root", 69.4),
+        ("output/zmumu_Np3.root", 21.6),
+        ("output/zmumu_Np4.root", 6.1),
+        ("output/zmumu_Np5.root", 1.7)
         ]
 
 names = ['Z mass', 'Z p_t', 'lepton p_t', 'transverse mass',
@@ -26,8 +26,16 @@ opts = [(100, 50, 150), (300, 0, 200), (300, 0, 200), (300, 0, 200),
 k_factor = 1.22
 
 if __name__ == '__main__':
+    get_count = lambda f: f.Get("count").GetUniqueID()
+
+    def write(obj):
+        out = ROOT.TFile("output/result.root", "UPDATE")
+        out.cd()
+        obj.Write()
+
     hists = dict(
-                (names[i], ROOT.TH1D(names[i], names[i], *opts[i]))
+                (names[i],
+                    ROOT.TH1D('[zmumu] ' + names[i], names[i], *opts[i]))
                 for i in xrange(len(names))
                 )
 
@@ -36,16 +44,25 @@ if __name__ == '__main__':
         
         root_file = ROOT.TFile(filename)
 
-        luminosity = root_file.Get("count").GetUniqueID() / cross_section
+        luminosity = get_count(root_file) / cross_section
         
         factor = target_luminosity / luminosity
 
         for name in names:
             hists[name].Add(root_file.Get(name), factor)
 
-    out = ROOT.TFile("output/joined_background.root", "RECREATE")
-    out.cd()
-
     for i in hists.itervalues():
-        i.Write()
+        write(i)
+
+    for prefix, index in (("[plus] ", 0), ("[minus] ", 1)):
+        root_file = ROOT.TFile(signal[index][0])
+        luminosity = get_count(root_file) / signal[index][1]
+        factor = target_luminosity / luminosity
+
+        for name in names:
+            histogram = root_file.Get(name)
+            histogram.SetName(prefix + name)
+            histogram.Scale(factor)
+            write(histogram)
+
 
